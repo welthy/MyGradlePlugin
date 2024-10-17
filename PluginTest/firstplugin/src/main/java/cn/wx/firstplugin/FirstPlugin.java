9b8475020b97f728309c88546af1df6229124949
welthy1210 <381217445@qq.com>
2024-10-17 22:29:17 +0800

    first plugin

---
+++ PluginTest/firstplugin/src/main/java/cn/wx/firstplugin/FirstPlugin.java
@@ -0,0 +1,16 @@
+package cn.wx.firstplugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+class FirstPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        project.getTasks().register("firstplugin", task -> {
+            task.doLast(s -> {
+                System.out.println("this is my first plugin");
+            });
+        });
+    }
+}
